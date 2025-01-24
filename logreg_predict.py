d70a4dc56fc6cd601dcc585bee96aa641d6283d9
sachadow <sachadow.team@gmail.com>
2025-01-24 22:26:25 +0100

    Added logreg_predict.py, taking functionality from logreg_train.py. Added a bunch of comments as well

---
+++ logreg_predict.py
@@ -0,0 +1,100 @@
+import numpy as np
+import describe
+import sys
+import argparse
+
+# Given a number X (which represents log(odds)), returns a value in between 0 and 1 which represents the probability
+# on the sigmoid function (with a nice S curve)
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+# Returns a prediction for each sample between 0 and 1
+def predict(X, weights, bias):
+    y_hat = np.dot(X, weights) + bias
+    y_predicted = sigmoid(y_hat)
+
+    return np.array(y_predicted)
+
+
+def open_weights_file(weights_file):
+    try:
+        with open(weights_file, 'r') as fd:
+            content = fd.read()
+    except FileNotFoundError:
+        print(f"{weights_file}: File not found.")
+        return False
+    
+    return content.split('\n')
+
+def main(file_to_test, weights_file, dest_file="houses.csv", verbose=False):
+    dest_file = "houses.csv" if dest_file is None else dest_file
+
+    names, file_content, lines = describe.open_file(file_to_test)
+
+    if not lines:
+        exit()
+
+    raw_weights = open_weights_file(weights_file)
+    if not raw_weights:
+        exit()
+
+    split_lines = []
+    for line in lines:
+        split_lines.append(line.split(","))
+
+    full_data = np.array(split_lines)
+    # Delete all fields that don't have numerical values (name, dominant hand, etc...)
+    full_data = np.delete(full_data, [0, 1, 2, 3, 4, 5], 1)
+    # Make any field with missing data 0, which isn't ideal but necessary (?)
+    full_data[full_data == ''] = 0
+    full_data = np.array(full_data, dtype=float)
+    # Normalize data between 0 and 10
+    # This is because values going too high can be very complicated to compute (especially with exponentials)
+    # and can cause trouble with the sigmoid function
+    full_data = (full_data - full_data.min(0)) / (np.ptp(full_data, axis=0) / 10)
+        
+    weights = {}
+    houses_names = []
+    for line in raw_weights:
+        split_weights = line.split(",")
+        if split_weights[0]:
+            houses_names.append(split_weights[0])
+            weights[split_weights[0]] = {
+                "bias": float(split_weights[1]),
+                "weights": np.array(split_weights[2:], dtype=float)
+            }
+    
+    full_predictions = []
+    
+    for house in weights.keys():
+        full_predictions.append(predict(full_data, weights[house]["weights"], weights[house]["bias"]))
+        if verbose:
+            print(f'Prediction done for {house}')
+
+    with open(dest_file, "w") as f:
+        f.write(f'Index,Hogwarts House\n')
+
+    full_predictions = np.array(full_predictions).argmax(axis=0)
+    for index, house_index in enumerate(full_predictions):
+        with open(dest_file, "a") as f:
+            f.write(f'{index},{houses_names[house_index]}\n')
+
+    if verbose:
+        print(f'Predictions written to {dest_file}')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_to_test", help="the file in .csv format to predict values for")
+    parser.add_argument("weights_file", help="the file in .lgr containing weights and biases")
+    parser.add_argument("-d", "--destination", help="the destination file for the predicted values")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+
+    args = parser.parse_args()
+
+    try:
+        main(args.file_to_test, args.weights_file, args.destination, args.verbose)
+    except Exception as e:
+        if (args.verbose):
+            print(f"{e}: invalid file formatting.")
+        else:
+            print(f"Invalid file formatting.")
--- logreg_train.py
+++ logreg_train.py
@@ -29,7 +29,7 @@ class LogisticRegression:
         return -np.mean(y1 + y2)
 
     # Calculates the a*x + b linear function in "sigmoid space" with a being our weights, b being our bias
-    # and X being our dataset
+    # and X being our full_dataset
     # The return will be an array of probabilities between 0 and 1
     def feed_forward(self, X):
         z = np.matmul(X, self.weights) + self.bias
@@ -38,7 +38,7 @@ class LogisticRegression:
 
     # y is an array of 1s and 0s, indicating whether the feature is True or False for any given sample
     # X is a 2D array of lines (samples) per columns (features) that can have any value
-    # X might need to be normalized depending on the data, but as long as the values aren't too high it should be fine
+    # X might need to be normalized depending on the full_data, but as long as the values aren't too large it should be fine
     def fit(self, X, y):
         n_samples, n_features = X.shape
 
@@ -58,47 +58,18 @@ class LogisticRegression:
             # compute gradients
             dw = (1 / n_samples) * np.dot(X.T, dz)
             db = (1 / n_samples) * np.sum(dz)
-            # update parameters
+
+            # Update weights and bias to go towards the local minimum
             self.weights -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
 
+    # Returns a prediction for each sample between 0 and 1
     def predict(self, X):
         y_hat = np.dot(X, self.weights) + self.bias
         y_predicted = self.sigmoid(y_hat)
         #y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
         
         return np.array(y_predicted)
-
-    def accuracy(self,y, y_hat):
-        accuracy = np.sum(y == y_hat) / len(y)
-        return accuracy
-
-
-def confusion_matrix(y_actual, y_predicted):
-    tp = 0
-    tn = 0
-    fp = 0
-    fn = 0
-    epsilon = 1e-9
-    for i in range(len(y_actual)):
-        if y_actual[i] > 0:
-            if y_predicted[i] > 0.5:
-                tp = tp + 1
-            else:
-                fn = fn + 1
-        if y_actual[i] < 1:
-            if y_predicted[i] < 0.5:
-                tn = tn + 1
-            else:
-                fp = fp + 1
-
-    cm = [[tp, fp], [tn, fn]]
-    accuracy = (tp+tn)/(tp+tn+fp+fn+epsilon)
-    sens = tp/(tp+fn+epsilon)
-    prec = tp/(tp+fp+epsilon)
-    f_score = (2*prec*sens)/(prec+sens+epsilon)
-    return cm,accuracy,sens,prec,f_score
-
 
 
 if __name__ == "__main__":
@@ -110,53 +81,42 @@ if __name__ == "__main__":
 
     houses_names = ["Gryffindor", "Ravenclaw", "Slytherin", "Hufflepuff"]
 
-    base_nplines = np.array(split_lines)
+    full_data = np.array(split_lines)
+    # Copy only the houses part of the data
+    actual_houses = full_data[:, 1]
 
-    full_predictions = []
+    # Delete all fields that don't have numerical values (name, dominant hand, etc...)
+    full_data = np.delete(full_data, [0, 1, 2, 3, 4, 5], 1)
+    # Make any field with missing data 0, which isn't ideal but necessary (?)
+    full_data[full_data == ''] = 0
+    full_data = np.array(full_data, dtype=float)
+    # Normalize data between 0 and 10
+    # This is because values going too high can be very complicated to compute (especially with exponentials)
+    # and can cause trouble with the sigmoid function
+    full_data = (full_data - full_data.min(0)) / (np.ptp(full_data, axis=0) / 10)
 
-    # Copy only the houses
-    houses_base = base_nplines[:, 1]
+    predictions_list = []
 
+    # Delete current contents of file
+    open("weights.lgr", "w").close()
+
+    # Test each house in one-vs-all
     for house_name in houses_names:
-        houses = np.array(houses_base)
+        # Set houses to either 0 or 1
+        # 1 means it's the house we're currently evaluating, 0 means it's any other
+        houses = np.array(actual_houses)
         houses[houses == house_name] = 1
         houses[houses != '1'] = 0
         houses = np.array(houses, dtype=float)
 
-        nplines = np.array(base_nplines)
-        nplines = np.delete(nplines, [0, 1, 2, 3, 4, 5], 1)
-        nplines[nplines == ''] = 0
-        nplines = np.array(nplines, dtype=float)
-
-        #print(nplines[0])
-        nplines = (nplines - nplines.min(0)) / (np.ptp(nplines, axis=0) / 10)
-        #print(nplines[0])
-
         regressor = LogisticRegression(learning_rate=0.0001, n_iters=10000)
-        regressor.fit(nplines, houses)
+        regressor.fit(full_data, houses)
 
-        predictions = regressor.predict(nplines)
-        #print(house_name)
-        #cm ,accuracy,sens,precision,f_score  = confusion_matrix(np.asarray(houses), np.asarray(predictions))
-        #print("Test accuracy: {0:.3f}".format(accuracy))
-        #print("Confusion Matrix:")
-        #print(np.array(cm))
+        # Write to file
+        # Format:
+        # house, bias, [weights (comma-separated)]
+        with open("weights.lgr", "a") as f:
+            f.write(f'{house_name},{regressor.bias},{','.join(map(str, regressor.weights))}\n')
 
-        full_predictions.append(predictions)
+    print(f'Weights and bias have been written to weights.lgr.')
 
-        #print(predictions[predictions > 0.5])
-
-    full_predictions = np.array(full_predictions).argmax(axis=0)
-    final_predictions = []
-    for prediction in full_predictions:
-        final_predictions.append(houses_names[prediction])
-
-    errors = np.where(final_predictions != base_nplines[:, 1])
-
-    for error in errors[0]:
-        print("Error: found", final_predictions[error], "where it was", base_nplines[:, 1][error], "on index", error)
-    
-    print("Error rate:", f'{(len(errors[0]) / len(base_nplines)) * 100: .2f}', "%")
-
-    #print(regressor.losses[0], regressor.losses[-1])
-    #print(regressor.weights, regressor.bias, regressor.losses)
